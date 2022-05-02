249a3d66089d9baf323726675eb52401c0225377
Daniel Adams <msub2official@gmail.com>
2022-05-02 01:22:45 -0400

    Add basic OpenGraph meta tags to front page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -18,6 +18,13 @@
 
 <svelte:head>
   <title>m₂ | Blog</title>
+  <meta name="description" content="The official blog for m₂, full of random musings and writings.">
+
+  <!-- OpenGraph Meta Tags -->
+  <meta property="og:url" content="https://blog.msub2.com/">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="m₂ Blog">
+  <meta property="og:description" content="The official blog for m₂, full of random musings and writings.">
 </svelte:head>
 
 <PostList posts={posts} />
