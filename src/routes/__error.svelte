99aae40307dfeaa3c5d3f03a7ef2852e976f97dc
Daniel Adams <msub2official@gmail.com>
2022-05-01 22:00:36 -0400

    Add generic error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+  export function load({ error, status }) {
+    return {
+      props: {
+        status: status,
+        error: error
+      }
+    };
+  }
+</script>
+
+<script>
+  export let status;
+  export let error;
+</script>
+
+<h1>{status}</h1>
+<h2>{error.message}</h2>
