f3d2f294cac25c984190608cf835845d6b601aae
Daniel Adams <msub2official@gmail.com>
2022-05-03 10:26:57 -0400

    Add page transitions

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,22 +1,34 @@
+<script context="module">
+  export const load = ({ url }) => {
+    const currentRoute = url.pathname
+
+    return {
+      props: {
+        currentRoute
+      }
+    }
+  }
+</script>
+
 <script>
   import Header from '$lib/components/Header.svelte';
   import Footer from '$lib/components/Footer.svelte';
   import '$lib/styles/style.css';
+  import { fade, fly } from 'svelte/transition'
+
+  export let currentRoute
 </script>
 
 <div class='wrapper'>
   <Header />
   
-  <main>
-    <slot />
-  </main>
+  {#key currentRoute}
+    <main in:fade={{ duration: 250, delay: 250 }} out:fade={{ duration: 250 }} 
+          on:outrostart="{() => document.body.style.overflowY = 'hidden'}" 
+          on:introend="{() => document.body.style.overflowY = 'auto'}">
+      <slot />
+    </main>
+  {/key}
   
   <Footer />
 </div>
-
-<style>
-  main {
-    display: flex;
-    flex-direction: column;
-  }
-</style>
