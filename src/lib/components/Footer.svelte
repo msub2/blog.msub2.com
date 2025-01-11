6b864201abf8a6492fe862cd3a9e2a7246dd7d3e
Daniel Adams <msub2official@gmail.com>
2025-01-11 11:05:20 -1000

    Update footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -9,14 +9,14 @@
 
 <footer>
   <hr />
-  <p id='copyright'>Copyright © 2024 m₂</p>
+  <p id='copyright'>Copyright © 2025 m₂</p>
   
   <nav>
     <a id='github' href='https://github.com/msub2' target='_blank' rel='noreferrer'>
       <img alt='GitHub Logo' src={github} width='64' height='64'/>
     </a>
-    <a id='twitter' href='https://www.twitter.com/msub2official' target='_blank' rel='noreferrer'>
-      <img alt='Twitter Logo' src={twitter} width='64' height='64' />
+    <a id="bluesky" rel="me noreferrer" href="https://bsky.app/profile/msub2.com" target="_blank">
+      <img alt="Bluesky Logo" src={bluesky} width="64" height="64">
     </a>
     <a id='youtube' href='https://www.youtube.com/channel/UCMVH8W8TRnzWFYVBQDqg93A' target='_blank' rel='noreferrer'>
       <img alt='YouTube Logo' src={youtube} width='64' height='64' />
@@ -24,8 +24,8 @@
     <a id='mastodon' rel="me noreferrer" href="https://arvr.social/@msub2" target='_blank'>
       <img alt='Mastodon Logo' src={mastodon} width='64' height='64' />
     </a>
-    <a id="bluesky" rel="me noreferrer" href="https://bsky.app/profile/msub2.com" target="_blank">
-      <img alt="Bluesky Logo" src={bluesky} width="64" height="64">
+    <a id='twitter' href='https://www.twitter.com/msub2official' target='_blank' rel='noreferrer'>
+      <img alt='Twitter Logo' src={twitter} width='64' height='64' />
     </a>
     <a id='rss' href='/rss.xml'>
       <img alt='RSS Feed' src={rss} width='64' height='64' />
